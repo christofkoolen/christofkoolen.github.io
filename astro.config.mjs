8d775a3abcd77ef0a83298490f96264e8dbca102
christofkoolen <christofkoolen@hotmail.com>
2026-07-18 18:14:07 +0200

    Fix root GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://christofkoolen.github.io',
-  base: '/academic-website',
   output: 'static',
   trailingSlash: 'never'
 });
